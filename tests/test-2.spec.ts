8f89f0d2978a871249a1d99d7f163604ab6ae177
Mourtada <smm7@njit.edu>
2023-12-13 20:54:07 -0500

    Playwright tests
    Home section Playwright test

--- tests/test-2.spec.ts
+++ tests/test-2.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
 test('check that Mourtada Mbaye is present', async ({ page }) => {
-  await page.goto('/');
+  await page.goto('/page.js');
   await page.getByRole('heading', { name: 'Mourtada Mbaye' }).click();
 });
